1eed851b3ed3ac229affdcab41c798c97c60e71a
Adel Ragab <ar1746@fayoum.edu.eg>
2023-12-12 11:29:06 +0200

    remove container and use textfield direct

--- lib/Features/login/view/widget/custom_text_field.dart
+++ lib/Features/login/view/widget/custom_text_field.dart
@@ -1,8 +1,10 @@
 import 'package:doc_appointment/cors/Themeing/colors.dart';
+import 'package:doc_appointment/cors/Themeing/styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hintText, this.onChanged, this.keyboardType});
+  const CustomTextField(
+      {super.key, required this.hintText, this.onChanged, this.keyboardType});
   final String hintText;
   final void Function(String)? onChanged;
   final TextInputType? keyboardType;
@@ -12,41 +14,31 @@ class CustomTextField extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.only(right: 15),
-          child: Container(
-            width: 380,
-            height: 55,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            decoration: ShapeDecoration(
-              color: Color(0xFFFDFDFF),
-              shape: RoundedRectangleBorder(
-                side: BorderSide(width: 1, color: Color(0xFFECECEC)),
-                borderRadius: BorderRadius.circular(16),
-              ),
-            ),
-            child: TextField(
-              onChanged: onChanged,
-              keyboardType: keyboardType,
-              decoration: InputDecoration(
-                  hintText: hintText,
-                  hintStyle: TextStyle(color: ColorsManager.Text_input_gray),
+          child: TextField(
+            onChanged: onChanged,
+            keyboardType: keyboardType,
+            decoration: InputDecoration(
+                hintText: hintText,
+                hintStyle: TextStyle(color: ColorsManager.Text_input_gray),
 
-                  //suffixIcon: IconButton(icon: Icon(Icons.clear), onPressed: () {}),
+                //suffixIcon: IconButton(icon: Icon(Icons.clear), onPressed: () {}),
 
-                  // prefixIcon: IconButton(
-                  //   icon: Icon(Icons.search),
-                  //   onPressed: () {
-                  //     // Perform the search here
-                  //   },
-                  // ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide.none,
+                // prefixIcon: IconButton(
+                //   icon: Icon(Icons.search),
+                //   onPressed: () {
+                //     // Perform the search here
+                //   },
+                // ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: ColorsManager.primaryColor,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  )),
-            ),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFECECEC)),
+                  borderRadius: BorderRadius.circular(16),
+                )),
           ),
         ),
       ],
